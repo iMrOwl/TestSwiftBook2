5470ac555a30a9d854a807ee97d51f8433c7f549
Evg.Sychev <Evg.Sychev@x5.ru>
2022-07-30 11:25:13 +0300

    добавил экран профайла

---
+++ TestSwiftbook2/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  TestSwiftbook2
+//
+//  Created by Evg.Sychev on 30.07.2022.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+  
+}
